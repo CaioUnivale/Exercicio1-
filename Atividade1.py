d63966fdf76a9ae1c4f4a65607c54b2cfd40d7ab
Caio Dultra da Silva <117033616+Caiodultra15@users.noreply.github.com>
2023-09-18 09:09:05 -0300

    first commit

---
+++ Atividade1.py
@@ -0,0 +1,25 @@
+# exibe uma saudação na tela
+def saudacao():
+    print("Olá! Bem-vindoà demonstração de funções.")
+
+# Chamada da função
+saudacao()
+
+
+def cumprimentar(nome):
+    print(f"Olá, {nome}! Como você está?")
+
+
+nome_pessoa = "Alice"
+cumprimentar(nome_pessoa)
+
+
+def calcular_area_retangulo(comprimento, largura):
+    area = comprimento * largura
+    return area
+
+
+comprimento_retangulo = 5
+largura_retangulo = 3
+area_calculada = calcular_area_retangulo(comprimento_retangulo, largura_retangulo)
+print(f"A área do retângulo é: {area_calculada} unidades²")
